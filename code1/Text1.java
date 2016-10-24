e8f0245c9b7642fd84cc4febe248dd089015b286
martin360 <mr002903@gmail.com>
2016-10-24 10:15:06 -0700

    Add files via upload

---
+++ code1/Text1.java
@@ -0,0 +1,25 @@
+class Text1 {
+    public static void main (String a[]){
+            int i,ci;  // integer i
+            char c;  // charater c
+    i=7;
+    c='A';
+    String s = "this is a string";
+    System.out.println("-------------------------");
+    System.out.println(i);
+    System.out.println(c);
+    System.out.println(s);  
+        System.out.println("-------------------------");
+    //do some math on the variables    
+    i = i % 7; 
+    ci =(int)c; //type cast characters c
+        
+    System.out.println("-------------------------");
+    System.out.println(i);
+    System.out.println(ci);
+    ci = ci % 7;
+    System.out.println(ci);
+    
+    }
+}
+ 
